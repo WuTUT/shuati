a91b056fe6bc44a336572934937102e161c9dad0
WuTUT <762641171@qq.com>
2020-06-25 19:33:35 +0800

    1

--- AC/luogu/题单模板题/动态规划/树形dp2/树的直径/P5536.cpp
+++ AC/luogu/题单模板题/动态规划/树形dp2/树的直径/P5536.cpp
@@ -4,72 +4,85 @@
 #include <cstring>
 #include <vector>
 using namespace std;
-const int maxn = 1e5+5;
-int n,k;
+const int maxn = 1e5 + 5;
+int n, k;
 vector<int> v[maxn];
-int dep[maxn],f[maxn],mdep[maxn],ans[maxn];
-int maxdep,maxx;
-void dfs(int root,int fa){
-    if(dep[root]>maxdep){
-        maxdep=dep[root];
-        maxx=root;    
+int dep[maxn], f[maxn], mdep[maxn], ans[maxn];
+int maxdep, maxx;
+void dfs(int root, int fa)
+{
+    if (dep[root] > maxdep)
+    {
+        maxdep = dep[root];
+        maxx = root;
     }
-    for(int i=0;i<v[root].size();i++){
-        int to =v[root][i];
-        if(to==fa) continue;
-        dep[to]=dep[root]+1;
-        dfs(to,root);
-        
+    for (int i = 0; i < v[root].size(); i++)
+    {
+        int to = v[root][i];
+        if (to == fa)
+            continue;
+        dep[to] = dep[root] + 1;
+        dfs(to, root);
     }
 }
 
-void dfs2(int root ,int fa){
-    if(dep[root]>maxdep){
-        maxdep=dep[root];
-        maxx=root;    
+void dfs2(int root, int fa)
+{
+    if (dep[root] > maxdep)
+    {
+        maxdep = dep[root];
+        maxx = root;
     }
-    for(int i=0;i<v[root].size();i++){
-        int to =v[root][i];
-        if(to==fa) continue;
-        dep[to]=dep[root]+1;
-        f[to]=root;
-        dfs2(to,root);
-        
+    for (int i = 0; i < v[root].size(); i++)
+    {
+        int to = v[root][i];
+        if (to == fa)
+            continue;
+        dep[to] = dep[root] + 1;
+        f[to] = root;
+        dfs2(to, root);
     }
 }
-void dfs3(int root ,int fa){
-    mdep[root]=dep[root];
-    for(int i=0;i<v[root].size();i++){
-        int to =v[root][i];
-        if(to==fa) continue;
-        dep[to]=dep[root]+1;
-        dfs3(to,root);
+void dfs3(int root, int fa)
+{
+    mdep[root] = dep[root];
+    for (int i = 0; i < v[root].size(); i++)
+    {
+        int to = v[root][i];
+        if (to == fa)
+            continue;
+        dep[to] = dep[root] + 1;
+        dfs3(to, root);
         mdep[root] = max(mdep[root], mdep[to]);
     }
 }
-int main(){ 
-    freopen("data.in","r",stdin);
+int main()
+{
+    freopen("data.in", "r", stdin);
     cin >> n >> k;
-    int x,y;
-    for(int i=1;i<n;i++){
-       cin >> x >> y; 
-       v[x].push_back(y);
-       v[y].push_back(x);
+    int x, y;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
     }
-    dfs(1,0);
-    memset(dep,0,sizeof dep);
-    maxdep=0;
-    dfs2(maxx,0);
+    dfs(1, 0);
+    memset(dep, 0, sizeof dep);
+    maxdep = 0;
+    dfs2(maxx, 0);
     int midx = maxx;
-    for(int i=1;i<=(dep[maxx]+1)/2;i++){
+    for (int i = 1; i <= (dep[maxx] + 1) / 2; i++)
+    {
         midx = f[midx];
     }
-    dfs3(midx,0);
-    for(int i=1;i<=n;i++){
-        ans[i]=mdep[i]-dep[i];
+    dfs3(midx, 0);
+    for (int i = 1; i <= n; i++)
+    {
+        ans[i] = mdep[i] - dep[i];
     }
-    sort(ans+1,ans+n+1,greater<int>());
-    cout << ans[k+1]+1 <<endl;
+    sort(ans + 1, ans + n + 1, greater<int>());
+    cout << ans[k + 1] + 1 << endl;
     return 0;
 }
 //第一个点一定要包含在直径中，再考虑最小化，则一定是直径链的中点
